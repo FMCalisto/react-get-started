d405228667c1af112fba5760b11558c9c3375de3
FMCalisto <francisco_mcalisto@hotmail.com>
2016-07-04 19:10:42 +0100

    [UPDATE] Code

--- react-tutorial-master/public/scripts/my-tutorial.js
+++ react-tutorial-master/public/scripts/my-tutorial.js
@@ -89,15 +89,45 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+
+    getInitialState: function() {
+        return {
+            author: '', text: ''
+        };
+    },
+
+    handleAuthorChange: function(e) {
+        this.setState({
+            author: e.target.value
+        });
+    },
+
+    handleTextChange: function(e) {
+        this.setState({
+            text: e.target.value
+        });
+    },
+
     render: function() {
         return (
-            React.createElement(
-                'div',
-                {className: "commentForm"},
-                "Hello, world! I am a CommentForm."
-            )
+            <form className="commentForm">
+                <input
+                    typy="text"
+                    placeholder="Your name"
+                    value={this.state.author}
+                    onChange={this.handleAuthorChange}
+                />
+                <input
+                    typy="text"
+                    placeholder="Say something..."
+                    value={this.state.text}
+                    onChange={this.handleTextChange}
+                />
+                <input type="submit" value="Post" />
+            </form>
         );
     }
+
 })
 
 ReactDOM.render(
