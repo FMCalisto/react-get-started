a67a94cb25784fe0be49bd16bf640ed873c012ce
FMCalisto <francisco_mcalisto@hotmail.com>
2016-07-04 19:26:28 +0100

    [UPDATE] Code

--- react-tutorial-master/public/scripts/my-tutorial.js
+++ react-tutorial-master/public/scripts/my-tutorial.js
@@ -108,9 +108,20 @@ var CommentForm = React.createClass({
         });
     },
 
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var author = this.state.author.trim();
+        var text = this.state.text.trim();
+        if(!text || !author) {
+            return;
+        }
+        // TODO: send request to the server
+        this.setState({author: '', text: ''});
+    },
+
     render: function() {
         return (
-            <form className="commentForm">
+            <form className="commentForm" onSubmit={this.handleSubmit}>
                 <input
                     typy="text"
                     placeholder="Your name"
