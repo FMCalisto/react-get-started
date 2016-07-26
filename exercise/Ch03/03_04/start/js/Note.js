48fa4056eba1adef6f928f6a980bcf7f10820411
FMCalisto <francisco_mcalisto@hotmail.com>
2016-07-26 20:12:34 +0100

    [UPDATE] Handling Events

--- exercise/Ch03/03_04/start/js/Note.js
+++ exercise/Ch03/03_04/start/js/Note.js
@@ -1,5 +1,21 @@
 var Note = React.createClass({
+    
     render: function() {
+
+        return (
+
+            <div className="note">
+
+                <span>
+
+                    <button className="btn btn-primary glyphicon glyphicon-pencil"/>
+                    <button className="btn btn-danger glyphicon glyphicon-trash"/>
+
+                </span>
+
+            </div>
+
+        );
         
 });
 
