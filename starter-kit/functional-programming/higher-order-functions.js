ca1e13c49f599566ed15853b52c3961b5d2c1845
FMCalisto <francisco_mcalisto@hotmail.com>
2016-11-16 20:45:35 +0000

    [UPDATE] Filter

--- starter-kit/functional-programming/higher-order-functions.js
+++ starter-kit/functional-programming/higher-order-functions.js
@@ -15,10 +15,18 @@ var animals = [
     { name: 'Harold', species: 'fish' },
 ]
 
-var dogs = []
+// Functions are values
 
-for (var i = 0; i < animals.length; i++) {
-    if (animals[i].species === 'dog') {
-        dogs.push(animals[i])
-    }
+// var dogs = []
+
+// for (var i = 0; i < animals.length; i++) {
+//     if (animals[i].species === 'dog') {
+//         dogs.push(animals[i])
+//     }
+// }
+
+var isDog = function(animal) {
+    return animal.species === 'dogs'
 }
+
+var dogs = animals.filter(isDog)
