2cda77949c4ff6caf113d02f558c4295138a6214
FMCalisto <francisco_mcalisto@hotmail.com>
2016-11-16 20:23:42 +0000

    [UPDATE] Loop

--- starter-kit/functional-programming/higher-order-functions.js
+++ starter-kit/functional-programming/higher-order-functions.js
@@ -3,3 +3,22 @@
 var triple = function(x) {
     return x * 3
 }
+
+var waffle = triple
+
+waffle(30)
+
+var animals = [
+    { name: 'Fluffy', species: 'rabbit' },
+    { name: 'Caro', species: 'dog' },
+    { name: 'Hamilton', species: 'dog' },
+    { name: 'Harold', species: 'fish' },
+]
+
+var dogs = []
+
+for (var i = 0; i < animals.length; i++) {
+    if (animals[i].species === 'dog') {
+        dogs.push(animals[i])
+    }
+}
