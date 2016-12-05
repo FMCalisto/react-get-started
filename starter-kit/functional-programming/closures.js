7eff265823897d13fb45e65ac4dea4819c6c8509
FMCalisto <francisco_mcalisto@hotmail.com>
2016-12-05 23:48:17 +0000

    [CREATE] Closures Source File

---
+++ starter-kit/functional-programming/closures.js
@@ -0,0 +1,11 @@
+var me = 'Francisco Calisto';
+
+/*
+ *  JavaScript functionjs are clores
+ */
+
+function greetMe() {
+    console.log('Hello, ' + me + '!');
+}
+
+greetMe();
--- starter-kit/functional-programming/reduce-advanced.js
+++ starter-kit/functional-programming/reduce-advanced.js
@@ -1,5 +1,19 @@
 import fs from 'fs';
 
-var output = fs.readFileSync('data.txt', 'utf8');
+var output = fs.readFileSync('data.txt', 'utf8')
+    .trim()
+    .split('\n')
+    .map(line => line.split('\t'))
+    .reduce((customers, line) => {
 
-console.log('Sheet:\n', output);
+        customers[line[0]] = customers[line[0]] || []
+        customers[line[0]].push({
+            name: line[1],
+            price: line[2],
+            quantity: line[3]
+        })
+
+        return customers
+    }, {})
+
+console.log('Sheet:\n', JSON.stringify(output, null, 2));
