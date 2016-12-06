44ccc30dc8dc23c81b7f0fc1cc3014e0587ba221
FMCalisto <francisco_mcalisto@hotmail.com>
2016-12-06 21:47:14 +0000

    [CREATE] Currying Source

---
+++ starter-kit/functional-programming/currying.js
@@ -0,0 +1,22 @@
+// Currying
+
+// 1
+
+// let dragon = (name, size, element) =>
+//     name + ' is a ' +
+//     size + ' dragon that breathes ' +
+//     element + '!'
+
+// console.log(dragon('fluffykins', 'tiny', 'lightning'))
+
+// 2
+
+// let dragon =
+//     name =>
+//         size =>
+//             element =>
+//                 name + ' is a ' +
+//                 size + ' dragon that breathes ' +
+//                 element + '!'
+
+// console.log(dragon('fluffykins')('tiny')('lightning'))
