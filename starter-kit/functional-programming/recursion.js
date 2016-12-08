625991b4785714e819e9df078eb918a5df3d8076
FMCalisto <francisco_mcalisto@hotmail.com>
2016-12-08 13:59:32 +0000

    [UPDATE] Recursion First Function

--- starter-kit/functional-programming/recursion.js
+++ starter-kit/functional-programming/recursion.js
@@ -4,4 +4,12 @@
  * When a function calls it self until it doesn't.
  */
 
- 
+let countDownFrom = (num) => {
+    if (num === 0) {
+        return;
+    }
+    console.log(num)
+    countDownFrom(num - 1)
+}
+
+countDownFrom(10)
