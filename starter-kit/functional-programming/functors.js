c779935b1adb955c2fcf4b3c9857416e23aa1ef0
FMCalisto <francisco_mcalisto@hotmail.com>
2016-12-20 20:37:45 +0000

    [UPDATE] Functors File

--- starter-kit/functional-programming/functors.js
+++ starter-kit/functional-programming/functors.js
@@ -1,7 +1,20 @@
 //functors
 
+function stringFunctor(value, fn) {
+    var chars = value.split("")
+    return chars.map(function(char) {
+        return String.fromCharCode(fn(char.charCodeAt(0)))
+    }).join("")
+}
+
 function plus1(value) {
     return value + 1;
 }
 
-console.log(plus1);
+function minus1(value) {
+    return value - 1;
+}
+
+console.log("return 1: ", [3, 4].map(plus1)); // returns [4, 5]
+console.log("return 2: ", stringFunctor("ABC", plus1)); // returns "BCD"
+console.log("return 3: ", stringFunctor("XYZ", minus1)); // returns "RXY"
