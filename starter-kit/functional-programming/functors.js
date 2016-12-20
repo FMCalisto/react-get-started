3bdd811f389778de06a465dbb53dc7b29527e3db
FMCalisto <francisco_mcalisto@hotmail.com>
2016-12-20 20:38:37 +0000

    [UPDATE] My Functors Num Function

--- starter-kit/functional-programming/functors.js
+++ starter-kit/functional-programming/functors.js
@@ -7,6 +7,10 @@ function stringFunctor(value, fn) {
     }).join("")
 }
 
+function numFunctor(arr, fn) {
+    return arr.map(fn);
+}
+
 function plus1(value) {
     return value + 1;
 }
@@ -15,6 +19,6 @@ function minus1(value) {
     return value - 1;
 }
 
-console.log("return 1: ", [3, 4].map(plus1)); // returns [4, 5]
+console.log("return 1: ", numFunctor([3, 4], plus1)); // returns [4, 5]
 console.log("return 2: ", stringFunctor("ABC", plus1)); // returns "BCD"
 console.log("return 3: ", stringFunctor("XYZ", minus1)); // returns "RXY"
