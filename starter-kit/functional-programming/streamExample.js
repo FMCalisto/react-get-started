4dd8f5cbb10f21b59240d09edf79f05c5f81d732
FMCalisto <francisco_mcalisto@hotmail.com>
2016-12-29 19:28:16 +0000

    [UPDATE] Stream Example My Map

--- starter-kit/functional-programming/streamExample.js
+++ starter-kit/functional-programming/streamExample.js
@@ -3,4 +3,20 @@ const highland = require('highland')
 
 highland(fs.createReadStream('customers.csv', 'utf8'))
   .split()
-  .each(x => console.log('each: ' + x))
+  .map(line => line.split(','))
+  .map(parts => ({
+    name: parts[0],
+    numPurchases: parts[1]
+  }))
+  .filter( customer => customer.numPurchases > 2 )
+  .each(x => console.log('each: ', x))
+
+const variables = [
+ { option: '1', value: 1 },
+ { option: '2', value: 2 },
+ { option: '3', value: 3 }
+]
+
+variables
+  .map((variable) => variable.value)
+  .forEach(printer => console.log("TESTING: ", printer))
