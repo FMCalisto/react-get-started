4cca1d5bd846fb8a7c5e8e832abdf5a1575da6d7
Francisco Maria Calisto <email@franciscocalisto.me>
2017-01-25 21:10:23 +0000

    [UPDATE] Statelesse

--- react-fundamentals/simple-react-app/react-app/src/App.js
+++ react-fundamentals/simple-react-app/react-app/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return <h1>Hello World</h1>
-  }
-}
+// Do not have state
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello World</h1>
+//   }
+// }
+
+// Do have state
+// class App extends React.Component {
+//   render() {
+//     return React.createElement('h1', null, 'Hello New');
+//   }
+// }
+
+// Do not have state
+const App = () => <h1>Hello Stateless</h1>
 
 export default App
