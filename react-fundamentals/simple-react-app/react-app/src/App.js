ee2557d9b21f8f07b2c207020e1a873f5a033b08
Francisco Maria Calisto <email@franciscocalisto.me>
2017-01-26 19:33:15 +0000

    [UPDATE] Access Nasted Data

--- react-fundamentals/simple-react-app/react-app/src/App.js
+++ react-fundamentals/simple-react-app/react-app/src/App.js
@@ -1,43 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      txt: 'this is my state',
-      cat: 0
-    }
-  }
-
-  update(e) {
-    this.setState({txt: e.target.value});
-  }
-
   render() {
-    let txt = this.props.txt;
-    return (
-      <div>
-        <Widget update={this.update.bind(this)} />
-        <Widget update={this.update.bind(this)} />
-        <Widget update={this.update.bind(this)} />
-        <h1>{this.state.txt}</h1>
-      </div>
-    )
+    return <Button>I <Heart /> React</Button>
   }
 }
 
-const Widget = (props) =>
-  <input type="text" onChange={props.update}/>
+const Button = (props) =>
+  <button>{props.children}</button>
 
-// Do have state
-// class App extends React.Component {
-//   render() {
-//     return React.createElement('h1', null, 'Hello New');
-//   }
-// }
+class Heart extends React.Component {
+  render() {
+    return <span>&hearts;</span>
+  }
+}
 
-// Do not have state
-// const App = () => <h1>Hello Stateless</h1>
+
 
 export default App
