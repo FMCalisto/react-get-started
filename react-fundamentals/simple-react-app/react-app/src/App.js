32d880dddb303ed57b95e7f891d2b79da117ce58
Francisco Maria Calisto <email@franciscocalisto.me>
2017-01-26 20:01:53 +0000

    [UPDATE] Normalize Events

--- react-fundamentals/simple-react-app/react-app/src/App.js
+++ react-fundamentals/simple-react-app/react-app/src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
 
 class App extends React.Component {
-  render() {
-    return <Title text="The Text" />
-  }
-}
 
-const Title = (props) =>
-  <h1>Title: {props.text}</h1>
-
-Title.propTypes = {
-  text (props, propName, component) {
-    if (!(propName in props)) {
-      return new Error (`Missing ${propName}`);
-    }
+  constructor () {
+    super();
+    this.state = {currentEvent: "---"}
+    this.update = this.update.bind(this)
   }
+
+  update (event) {
+    this.setState({currentEvent: event.type})
+  }
+
+  render () {
+    return (
+      <div>
+        <textArea
+          onKeyPress={this.update}
+          onCopy={this.update}
+          onCut={this.update}
+          onPaste={this.update}
+          cols="30"
+          rows="10"
+        />
+        <h1>{this.state.currentEvent}</h1>
+      </div>
+    )
+  }
+
 }
 
 export default App
