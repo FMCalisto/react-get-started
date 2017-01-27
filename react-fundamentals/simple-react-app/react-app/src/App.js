8c4744c2862691262dd6d897e9c30908409bbb29
Francisco Maria Calisto <email@franciscocalisto.me>
2017-01-27 19:17:39 +0000

    [UPDATE] Get a Reference to Specific Components

--- react-fundamentals/simple-react-app/react-app/src/App.js
+++ react-fundamentals/simple-react-app/react-app/src/App.js
@@ -4,26 +4,33 @@ class App extends React.Component {
 
   constructor () {
     super();
-    this.state = {currentEvent: "---"}
-    this.update = this.update.bind(this)
+    this.state = {a: ''}
+    this.state = {b: ''}
   }
 
-  update (event) {
-    this.setState({currentEvent: event.type})
+  update () {
+    this.setState({
+      a: this.a.value,
+      b: this.b.value
+    })
   }
 
   render () {
     return (
       <div>
-        <textArea
-          onKeyPress={this.update}
-          onCopy={this.update}
-          onCut={this.update}
-          onPaste={this.update}
-          cols="30"
-          rows="10"
+        <input
+          ref={node => this.a = node}
+          text="text"
+          onChange={this.update.bind(this)}
         />
-        <h1>{this.state.currentEvent}</h1>
+          {this.state.a}
+        <hr />
+        <input
+          ref={node => this.b = node}
+          text="text"
+          onChange={this.update.bind(this)}
+        />
+          {this.state.b}
       </div>
     )
   }
